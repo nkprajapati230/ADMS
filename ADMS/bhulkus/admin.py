00382f3c4ce31d7c02448fddcff01e2a95dfd073
Nitesh Prajapati <nkprajapati230@gmail.com>
2024-08-16 22:22:05 +0530

    fixed error

--- ADMS/bhulkus/admin.py
+++ ADMS/bhulkus/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from bhulkus.models import bhulku
 
 admin.site.register(bhulku)
+
+
--- ADMS/bhulkus/models.py
+++ ADMS/bhulkus/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class bhulku(models.Model):
+    id=models.IntegerField(primary_key=True,unique=True)
     first_name=models.CharField(max_length=24)
     middle_name=models.CharField(max_length=24)
     last_name=models.CharField(max_length=24)
 
     standard=models.SmallIntegerField(null=True)
     dob=models.DateField(null=True)
-    age=models.SmallAutoField(null=True)
+    age=models.SmallIntegerField(null=True)
 
     school=models.CharField(max_length=24,null=True)
     address=models.CharField(max_length=24,null=True)
