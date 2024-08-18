229f4217ecd56e7b453b1a8806a2944af43862cc
Nitesh Prajapati <nkprajapati230@gmail.com>
2024-08-18 12:01:06 +0530

    created base template

--- ADMS/ADMS/urls.py
+++ ADMS/ADMS/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from home import home_urls
+from leaders import leaders_urls
+from bhulkus import bhulkus_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(home_urls)),
+    path('leaders/',include(leaders_urls)),
+    path('bhulkus/',include(bhulkus_urls)),
 ]
